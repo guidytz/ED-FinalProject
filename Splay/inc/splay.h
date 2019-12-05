b6fb1ae891e73282601159b82411e88328107457
Garren Souza <contato.garren@gmail.com>
2019-12-05 16:42:57 -0300

    Update splay.h

--- Splay/inc/splay.h
+++ Splay/inc/splay.h
@@ -12,10 +12,10 @@ long rotacoes, comparacoes;
 
 /**
  * @brief 
- * Função que cria um novo nodo na árvore, retornando a referência para este novo nodo logo após a execução.
- * @param palavra 
- * @param esq 
- * @param dir 
+ * Função que cria um novo nodo na árvore, associando as dúas árvores recebidas como seus filhos.
+ * @param palavra Palavra que se deseja inserir no nodod que sera criado
+ * @param esq Árvore que será inserida na referência à esquerda do novo nodo
+ * @param dir Árvore que será inserida na referência à direita do novo nodo
  * @return SplayTree* 
  */
 SplayTree* novo_nodo(char* palavra, SplayTree* esq, SplayTree* dir);
@@ -23,8 +23,8 @@ SplayTree* novo_nodo(char* palavra, SplayTree* esq, SplayTree* dir);
 /**
  * @brief 
  * Função que insere um novo nodo na árvore, retornando uma referência à árvore já atualizada após a execução.
- * @param palavra 
- * @param nodo 
+ * @param palavra Palavra que se deseja inserir na árvore 
+ * @param nodo Nodo à partir do qual será realizado o procedimento de busca do local apropriado, dentro da árvore, no qual a nova palavra será inserida
  * @return SplayTree* 
  */
 SplayTree* insere_nodo(char* palavra, SplayTree* nodo);
@@ -32,8 +32,8 @@ SplayTree* insere_nodo(char* palavra, SplayTree* nodo);
 /**
  * @brief 
  * Função que realiza a rotação de nodos o "splay"
- * @param nodo 
- * @param palavra 
+ * @param nodo Nodo à partir do qual será realizado o "splay"
+ * @param palavra Palavra sobre a qual será realizado o splay
  * @return SplayTree* 
  */
 SplayTree* splay(SplayTree* nodo, char* palavra);
@@ -41,8 +41,8 @@ SplayTree* splay(SplayTree* nodo, char* palavra);
 /**
  * @brief 
  * Função auxiliar que realiza a consulta de um nodo na árvore, retornando uma referência à instância já atualizada.
- * @param nodo 
- * @param palavra 
+ * @param nodo Nodo à partir do qual será realizada a busca
+ * @param palavra Palavra que está sendo buscada dentro da árvore
  * @return SplayTree* 
  */
 SplayTree* consulta_arvore(SplayTree* nodo, char* palavra);
@@ -50,8 +50,8 @@ SplayTree* consulta_arvore(SplayTree* nodo, char* palavra);
 /**
  * @brief 
  * Função que remove um nodo da árvore, retornando uma referência à instância já atualizada.
- * @param nodo 
- * @param palavra 
+ * @param nodo Nodo à partir do qual será realizado o processo de pesquisa e de remoção da palavra em questão
+ * @param palavra Palavra que se deseja remover da árvore
  * @return SplayTree* 
  */
 SplayTree* remove_nodo(SplayTree* nodo, char* palavra);
@@ -59,126 +59,127 @@ SplayTree* remove_nodo(SplayTree* nodo, char* palavra);
 /**
  * @brief 
  * Função sem retorno que destroi a arvore, liberando a memória ocupada pela mesma
- * @param nodo 
+ * @param nodo Nodo à partir do qual será realizado a liberação de memória (destruição dos dados)
  */
 void destroi_arvore(SplayTree* nodo);
 
 /**
  * @brief 
- * Função que realiza a coleta 
- * @param lower_bound 
- * @param higher_bound 
+ * Função que realiza a coleta de palavras contidas pela arvore que possuem frequencia incçuída no intervalo em questão
+ * @param lower_bound Valor de teto utilizado na busca de palavras
+ * @param higher_bound Valor de piso utilizado na busca de palavras
  * @return char** 
  */
 char** contador(int lower_bound, int higher_bound);
 
 /**
  * @brief 
- * 
- * @param nodo 
- * @return int 
+ *  Função que realiza a contagem da quantidade de nodos contidos pela árvore no momento da chamada
+ * @param nodo Nodo à partir do qual será realizada a contagem
+ * @return int Quantidade de nodos da árvore 
  */
 int conta_nodos(SplayTree* nodo);
 
 /**
  * @brief 
  * Função que calcula a altura de um nodo dentro da árvore
- * @param nodo 
- * @return int 
+ * @param nodo Nodo à partir do qual será realizado o cálculo de altura
+ * @return int Altura do nodo
  */
 int altura(SplayTree* nodo);
 
 /**
  * @brief 
- * Função que encontra o maior fator de uma dada árvore
- * @param nodo 
- * @return int 
+ * Função que calcula o fator de um nodo
+ * @param nodo Nodo com relação ao qual se busca obter o fator
+ * @return int Fator do nodo
  */
 int fator(SplayTree* nodo);
 
 /**
  * @brief 
  * Função que retorna a frequência de uma dada palavra dentro da árvore
- * @param palavra 
- * @return int 
+ * @param palavra A palavra cuja frequência deseja-se descobrir
+ * @return int A frequência da palavra em questão
  */
 int frequencia(char* palavra);
 
 /**
  * @brief 
  * Função que realiza a soma das frequencias de todas as palavras inseridas na árvore até o momento
- * @param nodo 
- * @return int 
+ * @param nodo Nodo à partir do qual será realizada a soma de frequências das palavras
+ * @return int Soma das frequências das palavras contidas pela árvore em questão
  */
 int soma_frequencias(SplayTree* nodo);
 
 /**
  * @brief 
  * Encontra o maior fator à partir de um dado nodo
- * @param nodo 
- * @return int 
+ * @param nodo Nodo à partir do qual será realizado o procedimento de obtenção do maior fator contido pela árvore
+ * @return int O maior fator contido pela árvore recebida
  */
 int maior_fator(SplayTree* nodo);
 
 /**
  * @brief Get the 'rotacoes' object
  * Retorna a quantidade de rotações executadas até o momento
- * @return long 
+ * @return long Quantidade de rotações realizadas pela árvore até o momento
  */
 long get_rotacoes();
 
 /**
  * @brief Get the 'comparacoes' object
  * Retorna a quantidade de comparações realizadas até o momento
- * @return long 
+ * @return long Quantidade de comparações realizadas pela árvore até o momento
  */
 long get_comparacoes();
 
 /**
  * @brief 
  * Função de debug, realiza um caminhamento Central-Esquerdo imprimindo as informações de cada nodo
- * @param nodo 
+ * @param nodo Nodo à partir do qual será realizado o caminhamento
  */
 void caminha_ECD(SplayTree* nodo);
 
 /**
  * @brief Get the 'maior' object
  * Retorna o maior dos três parâmetros recebidos
- * @param a 
- * @param c 
- * @return int 
+ * @param a Valor inteiro 
+ * @param b Valor inteiro
+ * @param c Valor inteiro
+ * @return int Maior valor dentre aqueles recebidos
  */
-int get_maior(int a, int , int c);
+int get_maior(int a, int b, int c);
 
 /**
  * @brief 
  * Realiza uma rotação à esquerda de um dado nodo
- * @param nodo 
- * @return SplayTree* 
+ * @param nodo Nodo sobre o qual se deseja realizar a rotação
+ * @return SplayTree* Referência para a árvore resultante do processo de rotação
  */
 SplayTree* rot_esquerda(SplayTree* nodo);
 
 /**
  * @brief 
  * Realiza uma rotação à direita de um dado nodo
- * @param nodo 
- * @return SplayTree* 
+ * @param nodo Nodo sobre o qual se deseja realizar a rotação
+ * @return SplayTree* Referência para a árvore resultante do processo de rotação
  */
 SplayTree* rot_direita(SplayTree* nodo);
 
 /**
  * @brief 
  * Imprime uma representação de uma dada árvore à partir de um certo nodo (Créditos: Renata Galante)
- * @param nodo 
- * @param nivel 
+ * @param nodo Nodo à partir do qual deseja-se "desenhar" 
+ * @param nivel Nível do nodo de partida (usado apenas para fins de impressão)
  */
 void Desenha(SplayTree* nodo , int nivel);
 
 /**
  * @brief 
- * Função que de fato realiza a consulta de um nodod dentro de uma dada árvore
- * @param nodo 
- * @param palavra 
- * @return SplayTree* 
+ * Função que de fato realiza a consulta de um nodo dentro de uma dada árvore
+ * @param nodo Nodo à partir do qual será realizada a consulta
+ * @param palavra Palavra que está sendo buscada
+ * @return SplayTree* Referência da árvore após a realização da pesquisa
  */
 SplayTree* consulta_splay(SplayTree* nodo, char* palavra);
